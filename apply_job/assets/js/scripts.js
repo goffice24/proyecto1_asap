6357144b4647dbbb2cc4268f69602a88576ff9b6
andres <jatlhappy@gmail.com>
2022-07-28 11:48:14 -0700

    Asap

---
+++ apply_job/assets/js/scripts.js
@@ -0,0 +1,135 @@
+function bar_progress(progress_line_object, direction) {
+    var number_of_steps = progress_line_object.data('number-of-steps');
+    var now_value = progress_line_object.data('now-value');
+    var new_value = 0;
+    if (direction == 'right') {
+        new_value = now_value + (100 / number_of_steps);
+    } else if (direction == 'left') {
+        new_value = now_value - (100 / number_of_steps);
+    }
+    progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
+}
+
+jQuery(document).ready(function() {
+
+
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+
+    $('#top-navbar-1').on('shown.bs.collapse', function() {
+        $.backstretch("resize");
+    });
+    $('#top-navbar-1').on('hidden.bs.collapse', function() {
+        $.backstretch("resize");
+    });
+
+    /*
+        Form
+    */
+    $('.f1 fieldset:first').fadeIn('slow');
+
+    $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function() {
+        $(this).removeClass('input-error');
+    });
+
+    // next step
+    $('.f1 .btn-next').on('click', function() {
+        var parent_fieldset = $(this).parents('fieldset');
+        var next_step = true;
+        // navigation steps / progress steps
+        var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+        // fields validation
+        parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+            if ($(this).val() == "") {
+                $(this).addClass('input-error');
+                next_step = false;
+            } else {
+                $(this).removeClass('input-error');
+            }
+        });
+        // fields validation
+
+        if (next_step) {
+            parent_fieldset.fadeOut(400, function() {
+                // change icons
+                current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                // progress bar
+                bar_progress(progress_line, 'right');
+                // show next step
+                $(this).next().fadeIn();
+                // scroll window to beginning of the form
+                scroll_to_class($('.f1'), 20);
+            });
+        }
+
+    });
+
+    // previous step
+    $('.f1 .btn-previous').on('click', function() {
+        // navigation steps / progress steps
+        var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+        $(this).parents('fieldset').fadeOut(400, function() {
+            // change icons
+            current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+            // progress bar
+            bar_progress(progress_line, 'left');
+            // show previous step
+            $(this).prev().fadeIn();
+            // scroll window to beginning of the form
+            scroll_to_class($('.f1'), 20);
+        });
+    });
+
+    // submit
+    $('.f1').on('submit', function(e) {
+
+        // fields validation
+        $(this).find('input[type="text"], input[type="password"], textarea').each(function() {
+            if ($(this).val() == "") {
+                e.preventDefault();
+                $(this).addClass('input-error');
+            } else {
+                $(this).removeClass('input-error');
+            }
+        });
+        // fields validation
+
+    });
+
+
+});
+
+
+'use strict';
+
+;
+(function(document, window, index) {
+    var inputs = document.querySelectorAll('.inputfile');
+    Array.prototype.forEach.call(inputs, function(input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener('change', function(e) {
+            var fileName = '';
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else
+                fileName = e.target.value.split('\\').pop();
+
+            if (fileName)
+                label.querySelector('span').innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+
+        // Firefox bug fix
+        input.addEventListener('focus', function() { input.classList.add('has-focus'); });
+        input.addEventListener('blur', function() { input.classList.remove('has-focus'); });
+    });
+}(document, window, 0));
